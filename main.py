f85e80cd8a2f491f013a5e07d4a01a8b6e66a956
OmarTamer177 <110217240+OmarTamer177@users.noreply.github.com>
2024-02-22 17:04:33 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+import pygame
+import sys
+from Settings import *
+
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.mid_flip = pygame.image.load('graphics/yellowbird-midflap.png').convert_alpha()
+        self.up_flip = pygame.image.load('graphics/yellowbird-upflap.png').convert_alpha()
+        self.down_flip = pygame.image.load('graphics/yellowbird-downflap.png').convert_alpha()
+        self.frames = [self.up_flip, self.mid_flip, self.down_flip]
+        for index in range(0, len(self.frames)):
+            self.frames[index] = pygame.transform.rotozoom(self.frames[index], 0, BACKGROUND_MULTIPLIER)
+
+        self.frame_index = 0
+
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_rect(midbottom=(100, 300))
+
+    def bird_animation(self):
+        self.frame_index += 0.06
+        if self.frame_index >= len(self.frames):
+            self.frame_index = 0
+        self.image = self.frames[int(self.frame_index)]
+
+    def bird_gravity(self):
+        pass
+
+    def update(self):
+        self.bird_animation()
+
+
+#initiate pygame, screen and clock
+pygame.init()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+clock = pygame.time.Clock()
+
+#Background surface initialization, and scale it to fit screen
+background = pygame.image.load('graphics/background-day.png').convert_alpha()
+background = pygame.transform.rotozoom(background, 0, BACKGROUND_MULTIPLIER)
+
+#A user-event responsible for toggling day and night
+background_toggle = pygame.USEREVENT
+pygame.time.set_timer(background_toggle, 10000)
+is_day = True
+
+#Create the player object
+bird = pygame.sprite.GroupSingle()
+bird.add(Bird())
+
+#############Game loop#######################
+while True:
+    #########event loop######################
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        #toggle background between day and night
+        if event.type == background_toggle:
+            if is_day:
+                background = pygame.image.load('graphics/background-night.png').convert_alpha()
+                background = pygame.transform.rotozoom(background, 0, BACKGROUND_MULTIPLIER)
+            else:
+                background = pygame.image.load('graphics/background-day.png').convert_alpha()
+                background = pygame.transform.rotozoom(background, 0, BACKGROUND_MULTIPLIER)
+            is_day = not is_day
+
+    #Draw background
+    screen.blit(background, (0, 0))
+
+    #drawing and updating game elements
+    bird.draw(screen)
+    bird.update()
+
+    pygame.display.update()
+    clock.tick(FRAME_RATE)
